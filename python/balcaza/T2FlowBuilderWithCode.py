708eb4febd473e00f3666016e7347fd487bfb6de
Simone Bna <s.bna@scsitaly.com>
2015-11-02 17:12:35 +0100

    first dirty implementation of chic-mml-2-t2flow converter

--- python/balcaza/T2FlowBuilderWithCode.py
+++ python/balcaza/T2FlowBuilderWithCode.py
@@ -42,6 +42,24 @@ def printSig(sourceFile, flowName):
     sys.stdout.write(")\n")
 
 
+class Terminal:
+  
+    def __init__(self, kind=None):
+      self._ports = {'_out' : {}, '_in' : {}}
+      self._kind = kind
+    
+    def set_type(self, kind):
+      self._kind = kind
+    
+    def add_port(self, port_id, port_value):
+        if self._kind == 'source':
+          self._ports['_out'][port_id] = port_value
+        elif self._kind == 'sink':
+          self._ports['_in'][port_id] = port_value
+        else:
+	  raise Exception('Error: the kind of terminal is not set!')
+
+
 class T2FlowBuilder:
 
     def convert(self, sourceFile, t2flow, flowName, compressed, validate, zip):
@@ -62,57 +80,133 @@ class T2FlowBuilder:
         # create the workflow from the model
         flow = Workflow(title = xmml['model']['@name'], description = xmml['model']['description'])
         
+        # create tasks
         terminals = dict()
         nsubmodels = dict()
+        mytask = dict()
         
         nelements =  len (xmml['model']['definitions'])
         definitions = xmml['model']['definitions']
 
         for type_elem, elem in definitions.items():
            if type_elem == 'terminal':
+   	     if type(elem) is list:
                for x in elem:
-		   print len(elem)
+	         #print x
+	         terminal = Terminal(kind=x['@type'])
+	         if 'out' in x['ports'].keys():
+		   if type(x['ports']['out']) is list:
+		     for pp in x['ports']['out']:
+		       port_id = pp['@id']
+		       port_value = {'datatype' : pp['@datatype']}
+		       terminal.add_port(port_id, port_value)
+		   else:
+		     port_id = x['ports']['out']['@id']
+		     port_value = {'datatype' : x['ports']['out']['@datatype']}
+		     terminal.add_port(port)
+		 if 'in' in x['ports'].keys():
+		   if type(x['ports']['in']) is list:
+		     for pp in x['ports']['in']:
+		       port_id = pp['@id']
+		       port_value = {'datatype' : pp['@datatype']}
+		       terminal.add_port(port_id, port_value)
+		   else:
+		     port_id = x['ports']['in']['@id']
+		     port_value = {'datatype' : x['ports']['in']['@datatype']}
+		     terminal.add_port(port_id, port_value)
+	         terminal.add_port
+	         terminals[x['@id']] = terminal
+	     else:
+	       b = 2
            if type_elem == 'submodel':
-               print len(elem)
+	     if type(elem) is list:
+	       a = 2
+	     else:
+               implementation_code  = str(elem['implementation']['code']['#text'])
+               task_name = elem['@id']
+               if elem['@type'] == 'BeanshellCode':
+                 #print elem
+                 # ports
+                 #print elem['ports']['in']
+                 #loop over in-ports
+                 myinputs = dict()
+                 if type(elem['ports']['in']) is list:
+                   for in_ports in elem['ports']['in']:
+		     #print in_ports
+		     if in_ports['@datatype'] == 'string':
+                       myinputs[in_ports['@id']] = String
+                 else:
+		   if elem['ports']['in']['@datatype'] == 'string':
+		     myoutputs[elem['ports']['in']['@id']] = String
+                 
+                 #loop over out-ports
+		 myoutputs = dict()
+                 if type(elem['ports']['out']) is list:
+		   for out_ports in elem['ports']['out']:
+		     print out_ports
+		     if out_ports['@datatype'] == 'string':
+                       myoutputs[out_ports['@id']] = String
+		 else:
+		   if elem['ports']['out']['@datatype'] == 'string':
+		     myoutputs[elem['ports']['out']['@id']] = String
 
-		 
-        #terminals = dict()
-        #nterminals = len (xmml['model']['definitions']['terminal'])
-        #for x in range (0, nterminals):
-            #terminals[xmml['model']['definitions']['terminal'][x]['@id']] = xmml['model']['definitions']['terminal'][x]
-            
-        ##print terminals
+                 # BeanshellCode task
+		 bnshcode = flow.task[task_name] << BeanshellCode(implementation_code,
+			inputs = myinputs,
+			outputs = myoutputs
+	                 )
+		 mytask[task_name] = bnshcode
+	       else:
+		 raise Exception('The submodel type does not exist')
+
+
+        # link the tasks
+        #print terminals
+        #print xmml['model']['topology']
+        myinstances = dict()
+        # elem_top contains in two different dictionaries 
+        for type_top, elem_top in xmml['model']['topology'].items():
+          if type_top == 'instance':
+	    for instance in elem_top:
+	      #print instance
+	      #print instance['@id']
+	      if '@submodel' in instance.keys():
+	        single_instance = ('submodel', instance['@submodel'])
+	      elif '@terminal' in instance.keys():
+		single_instance = ('terminal', instance['@terminal'])
+	      myinstances[instance['@id']] = single_instance
+          if type_top == 'coupling':
+	    for coupling in elem_top:
+	      data_from = coupling['@from']
+	      data_from_instance = data_from.split(".")[0]
+	      data_from_port = data_from.split(".")[1]
+	      data_to = coupling['@to']
+	      data_to_instance = data_to.split(".")[0]
+	      data_to_port = data_to.split(".")[1]
+	      #print data_from
+	      #print data_to
+	      # coupling from a terminal to a submodel
+	      if myinstances[data_from_instance][0] == 'terminal' and myinstances[data_to_instance][0] == 'submodel':
+		#print terminals[myinstances[data_from_instance][1]]
+		#print mytask[myinstances[data_to_instance][1]]
+		if terminals[myinstances[data_from_instance][1]]._kind == 'source':
+		  data_left =  flow.input[data_from_port]
+		  data_right = mytask[myinstances[data_to_instance][1]].input[data_to_port]
+		  data_left | data_right 
+              # coupling from a submodel to a terminal
+              elif myinstances[data_from_instance][0] == 'submodel' and myinstances[data_to_instance][0] == 'terminal':
+	        if terminals[myinstances[data_to_instance][1]]._kind == 'sink':
+		  data_left = mytask[myinstances[data_from_instance][1]].output[data_from_port]
+		  data_right = flow.output[data_to_port] 
+		  data_left | data_right 
         
-        #submodels = dict()
-        #print xmml['model']['definitions']['submodel']
-        #nsubmodels = len (xmml['model']['definitions']['submodel'])
-        #print len (xmml['model']['definitions'])
-        ##for x in range (0, nsubmodels):
-	    ##print xmml['model']['definitions']['terminal'][x]['@type']
-        
-        bnshcode = flow.task.MyTask << BeanshellCode("""outputstr = string1 + string2;""",
-			inputs = dict(
-			  string1 = String,
-			  string2 = String
-			  ),
-			outputs = dict(
-			  outputstr = String
-			  )
-	  )
-
-        
-        flow.input.string1 | bnshcode.input.string1
-        flow.input.string2 | bnshcode.input.string2
-        
-        bnshcode.output.outputstr | flow.output.output_str        
-
-        #GetWebPage = flow.task.RetrieveWebPage << HTTP.GET('http://www.biovel.eu/')
-
-        #from balcaza.activity.local.list import MergeStringListToString
-
-        #GetWebPage | XPath('/xhtml:html/xhtml:head/xhtml:title', {'xhtml': 'http://www.w3.org/1999/xhtml'}) | MergeStringListToString | flow.output.title
-
-        #GetWebPage.output.responseHeaders | MergeStringListToString | flow.output.headers
+        #a = mytask['HW1']
+        #flow.input['stringaaa1'] | a.input['string1']
+        #flow.input['strinaaag2'] | a.input['string2']
+        #a = bnshcode.output.outputstr
+        #b = flow.output['output_str']
+        #a | b
+        #bnshcode.output.outputstr | flow.output.output_str    
         ##flow = readZapyFile(sourceFile, flowName)
 
         if validate:
